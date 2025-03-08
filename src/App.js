8552d74f970e72e13edbad4c7c35cd8c8689e59b
chromody <chromody@gmail.com>
2025-03-08 17:20:40 -0600

    bootstrap with code block

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { CodeBlock, dracula  } from 'react-code-blocks';
 
 function App() {
+  const helloWorld = "const greeting = 'Hello, world!';\nconsole.log(greeting);\n// Output: Hello, world!";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="light" expand="lg" className="shadow-lg">
+        <Container>
+          <Navbar.Brand className="fw-bold fs-3">
+            Jesus Villanueva-Segovia  
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container style={{ paddingTop: '30px' }}>
+        <div className="bg-dark p-3 rounded">
+          <CodeBlock
+          text={helloWorld}
+          language={"English"}
+          showLineNumbers={50}
+          theme={dracula}
+          />
+        </div>
+      </Container>
     </div>
   );
 }
