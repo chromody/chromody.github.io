d0ef3dd1df2fa74d2ed066e6a4fda38166aa4827
chromody <chromody@gmail.com>
2025-03-08 18:14:53 -0600

    made it type line by line

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { CodeBlock, dracula  } from 'react-code-blocks';
+import React, { useState, useEffect } from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import { CodeBlock, dracula } from 'react-code-blocks';
 
 function App() {
-  const helloWorld = "const greeting = 'Hello, world!';\nconsole.log(greeting);\n// Output: Hello, world!";
+  const [displayedText, setDisplayedText] = useState('');
+  const [charIndex, setCharIndex] = useState(0);
+  const [displayPopup, setDisplayPopup] = useState(false);
+  const greetings = `const greeting=👋Welcome!👋\nprint(greeting)\n// Output: Welcome!`;
+
+  useEffect(() => {
+    if (charIndex < greetings.length) {
+      const typingInterval = setInterval(() => {
+        setDisplayedText((prevText) => prevText + greetings[charIndex]);
+        setCharIndex((prevIndex) => prevIndex + 1);
+      }, 25); // Typing speed (100ms per character)
+
+      // Cleanup interval on component unmount
+      return () => clearInterval(typingInterval);
+    }
+    setDisplayPopup(true);
+  }, [charIndex]); // Trigger when charIndex changes
+
   return (
     <div>
       <Navbar bg="light" expand="lg" className="shadow-lg">
         <Container>
-          <Navbar.Brand className="fw-bold fs-3">
-            Jesus Villanueva-Segovia  
+          <Navbar.Brand className="fs-3 font-FiraCode">
+            Jesus Villanueva-Segovia
           </Navbar.Brand>
         </Container>
       </Navbar>
 
       <Container style={{ paddingTop: '30px' }}>
-        <div className="bg-dark p-3 rounded">
+        <div className="bg-dark p-3 rounded drop" style={{boxShadow: '0 4px 10px rgba(0, 0, 0, 0.3)'}}>
+          greetings.py
           <CodeBlock
-          text={helloWorld}
-          language={"English"}
-          showLineNumbers={50}
-          theme={dracula}
+            text={displayedText}
+            language="python"
+            showLineNumbers={50}
+            theme={dracula}
           />
         </div>
+
+        {
+          displayPopup && 
+            <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+              <Card bg='dark' className="mt-4 shadow-lg p-1 text-center" style={{ display: 'inline-block' }}>
+                <Card.Body>
+                  <Card.Text>
+                    👋 Welcome!👋
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+        }
       </Container>
     </div>
   );
