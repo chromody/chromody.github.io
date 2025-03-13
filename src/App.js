524f6657d44ab41c2efcf70a636e8b60340ac947
chromody <chromody@gmail.com>
2025-03-13 00:34:21 -0500

    updated routing for spa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
