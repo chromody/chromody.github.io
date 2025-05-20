72d65527eadd57f6cd2488abf3836539dfd97498
chromody <chromody@gmail.com>
2025-05-19 22:06:05 -0500

    careful

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,34 @@
+import { useState, useEffect } from "react";
 import Layout from './../layout/Layout';
+import { projectData } from "./../data/project";
+import ProjectCard from "./../components/ProjectCard";
 
 const Portfolio = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        async function getData() {
+            let projectInfo = await projectData();
+            setData(projectInfo);
+        }
+        getData().then(() => {
+            console.log(data);
+        })
+    }, [])
+
     return(
         <Layout>
-            Being Built
+            <div className=''>
+                {
+                    data.map( (project, i) => {
+                        console.log(project);
+                        return (
+                            <div key={i}>
+                                <ProjectCard imgSrc={project.imageSrc} highlightImage={project.highlightImage} link={project.link} title={project.title} description={project.description} />
+                            </div>
+                        );
+                    })
+                }
+            </div>
         </Layout>
     )
 }
