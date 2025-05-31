29b5550f16f7adfd8110e78dc13cc8fc63ef8c8d
Jesus Villanueva-Segovia <chromody@gmail.com>
2025-05-30 20:21:58 -0500

    fixed dead code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { useNavigate, useLocation } from "react-router-dom";
 
