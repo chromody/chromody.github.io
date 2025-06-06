f706af4b8e7e0c93e6338ccf7bb23f7422f983fa
Jesus Villanueva-Segovia <chromody@gmail.com>
2025-06-06 00:43:13 -0500

    added images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import SVG from "react-inlinesvg";
+import "./style/Icons.css"
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="bg-dark text-white text-center py-3 mt-auto w-auto h-auto">
+        <footer className="bg-dark text-white py-3 mt-auto w-auto h-auto">
             <Container>
                 <Row>
                     <Col>
-                        <p>© {currentYear} Jesus Villanueva-Segovia. All Rights Reserved.</p>
-                        <p>
-                            <a href="https://github.com/chromody" className="text-white" target="_blank" rel="noopener noreferrer">
-                            GitHub
-                            </a> | 
+                        <div className="">
+                            <span>Jesus Villanueva-Segovia</span>
+                            <br />
+                            <span style={{color: "#77a39d", fontSize: "14px"}}>Developer From United States</span>
+                        </div>
+                        <div className="pt-1 pb-1 d-flex align-items-center">
+                            <a href="https://github.com/chromody" className="text-white " target="_blank" rel="noopener noreferrer">
+                                <SVG className="svg-icon" src={"projectImages/Github_Invertocat_Dark.svg"}  style={{ height: "30px", width: "30px", fill: "#52e0c4"}} preProcessor={(code) => code.replace(/fill=".*?"/g, 'fill="currentColor"')} />
+                            </a> 
                             <a href="https://www.linkedin.com/in/jesusv-s/" className="text-white" target="_blank" rel="noopener noreferrer">
-                            LinkedIn
-                            </a> | 
+                                <SVG className="svg-icon" src={"icons8-linkedin.svg"}  style={{ height: "35px", width: "35px", fill: "#52e0c4"}} preProcessor={(code) => code.replace(/fill=".*?"/g, 'fill="currentColor"')} />
+                            </a> 
                             <a href="https://drive.google.com/file/d/1DOyr9Ph8uGpFo1pch7-HJeNHUjfc7-Sc/view?usp=drive_link" className="text-white" target="_blank" rel="noopener noreferrer" download>
-                            Resume
-                            </a> |
-                            <a href="/#/contact" className="text-white">
-                            Contact
+                                <SVG className="svg-icon" src={"resume.svg"}  style={{ height: "30px", width: "30px", fill: "#52e0c4"}} preProcessor={(code) => code.replace(/fill=".*?"/g, 'fill="currentColor"')} />
                             </a>
-                        </p>
+                            <a href="/#/contact" className="text-white">
+                                <SVG className="svg-icon" src={"email.svg"}  style={{ height: "30px", width: "30px", fill: "#52e0c4"}} preProcessor={(code) => code.replace(/fill=".*?"/g, 'fill="currentColor"')} />
+                            </a>
+                        </div>
+                        <span style={{fontSize: "10px"}}>© {currentYear} Jesus Villanueva-Segovia</span>
                     </Col>
                 </Row>
             </Container>
